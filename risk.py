9dbce5ee25eb7ac04f912c0e100e4ac6183a67a9
Rogier van der Geer <rogiervandergeer@godatadriven.com>
2015-12-16 16:09:00 +0100

    Complete rewrite.
    The classes now have a much better structure, which means they can
    be compared (Region(a) != Region(b), etc), and used in sets. Also
    Allocation() is plainly cool.

--- risk.py
+++ risk.py
@@ -1,220 +1,273 @@
 import os, random, math, copy
 import matplotlib.pyplot as plt
 
-graph = {
-    # North America
-    'alaska': ['northwest-territory', 'alberta', 'kamchatka'],
-    'alberta': ['alaska', 'northwest-territory', 'ontario', 'western-united-states'],
-    'central-america': ['western-united-states', 'eastern-united-states', 'venezuela'],
-    'eastern-united-states': ['central-america', 'western-united-states', 'ontario', 'quebec'],
-    'greenland': ['northwest-territory', 'ontario', 'quebec', 'iceland'],
-    'northwest-territory': ['alaska', 'alberta', 'greenland', 'ontario'],
-    'ontario': ['alberta', 'eastern-united-states', 'greenland', 'northwest-territory', 'quebec', 'western-united-states'],
-    'quebec': ['eastern-united-states', 'greenland', 'ontario'],
-    'western-united-states': ['alberta', 'central-america', 'eastern-united-states', 'ontario'],
-    # South America
-    'argentina': ['brazil', 'peru'],
-    'brazil': ['north-africa', 'argentina', 'peru', 'venezuela'],
-    'peru': ['brazil', 'venezuela', 'argentina'],
-    'venezuela': ['peru', 'brazil', 'central-america'],
-    # Europe
-    'great-britain': ['iceland', 'northern-europe', 'scandinavia', 'western-europe'],
-    'iceland': ['greenland', 'great-britain', 'scandinavia'],
-    'northern-europe': ['scandinavia', 'ukraine', 'southern-europe', 'western-europe', 'great-britain'],
-    'scandinavia': ['ukraine', 'northern-europe', 'great-britain', 'iceland'],
-    'southern-europe': ['egypt', 'western-europe', 'northern-europe', 'ukraine', 'middle-east'],
-    'ukraine': ['ural', 'afghanistan', 'middle-east', 'southern-europe', 'northern-europe', 'scandinavia'],
-    'western-europe': ['great-britain', 'north-africa', 'southern-europe', 'northern-europe'],
-    # Africa
-    'congo': ['east-africa', 'south-africa', 'north-africa'],
-    'east-africa': ['egypt', 'middle-east', 'madagascar', 'south-africa', 'congo', 'north-africa'],
-    'egypt': ['southern-europe', 'middle-east', 'east-africa', 'north-africa'],
-    'madagascar': ['east-africa', 'south-africa'],
-    'north-africa': ['western-europe', 'egypt', 'east-africa', 'congo', 'brazil'],
-    'south-africa': ['congo', 'east-africa', 'madagascar'],
-    # Asia
-    'afghanistan': ['ukraine', 'ural', 'china', 'india', 'middle-east'],
-    'china': ['siam', 'india', 'afghanistan', 'siberia', 'ural', 'mongolia'],
-    'india': ['china', 'siam', 'middle-east', 'afghanistan'],
-    'irkutsk': ['yakutsk', 'mongolia', 'siberia', 'kamchatka'],
-    'japan': ['kamchatka', 'mongolia'],
-    'kamchatka': ['japan', 'alaska', 'irkutsk', 'mongolia', 'yakutsk'],
-    'middle-east': ['east-africa', 'egypt', 'ukraine', 'afghanistan', 'india', 'southern-europe'],
-    'mongolia': ['japan', 'china', 'siberia', 'irkutsk', 'kamchatka'],
-    'siam': ['indonesia', 'india', 'china'],
-    'siberia': ['irkutsk', 'yakutsk', 'mongolia', 'china', 'ural'],
-    'ural': ['siberia', 'china', 'afghanistan', 'ukraine'],
-    'yakutsk': ['kamchatka', 'irkutsk', 'siberia'],
-    # Oceania,
-    'eastern-australia': ['new-guinea', 'western-australia'],
-    'indonesia': ['siam', 'new-guinea', 'western-australia'],
-    'new-guinea': ['eastern-australia', 'indonesia', 'western-australia'],
-    'western-australia': ['eastern-australia', 'indonesia', 'new-guinea']
-}
-continents = {
-    'north-america': ['alaska', 'alberta', 'central-america', 'eastern-united-states', 
-                      'greenland', 'northwest-territory', 'ontario', 'quebec', 'western-united-states'],
-    'south-america': ['argentina', 'brazil', 'peru', 'venezuela'],
-    'europe': ['great-britain', 'iceland', 'northern-europe', 'scandinavia', 'southern-europe', 
-               'ukraine', 'western-europe'],
-    'africa': ['congo', 'east-africa', 'egypt', 'madagascar', 'north-africa', 'south-africa'],
-    'asia': ['afghanistan', 'china', 'india', 'irkutsk', 'japan', 'kamchatka', 'middle-east', 
-             'mongolia', 'siam', 'siberia', 'ural', 'yakutsk'],
-    'oceania': ['eastern-australia', 'indonesia', 'new-guinea', 'western-australia']
-}
-continent_bonus = {
-    'north-america': 5,
-    'south-america': 2,
-    'europe': 5,
-    'africa': 3,
-    'asia': 7,
-    'oceania': 2
-}
+region_names = {
+     0: 'afghanistan',
+     1: 'alaska',
+     2: 'alberta',
+     3: 'argentina',
+     4: 'brazil',
+     5: 'central-america',
+     6: 'china',
+     7: 'congo',
+     8: 'east-africa',
+     9: 'eastern-australia',
+     10: 'eastern-united-states',
+     11: 'egypt',
+     12: 'great-britain',
+     13: 'greenland',
+     14: 'iceland',
+     15: 'india',
+     16: 'indonesia',
+     17: 'irkutsk',
+     18: 'japan',
+     19: 'kamchatka',
+     20: 'madagascar',
+     21: 'middle-east',
+     22: 'mongolia',
+     23: 'new-guinea',
+     24: 'north-africa',
+     25: 'northern-europe',
+     26: 'northwest-territory',
+     27: 'ontario',
+     28: 'peru',
+     29: 'quebec',
+     30: 'scandinavia',
+     31: 'siam',
+     32: 'siberia',
+     33: 'south-africa',
+     34: 'southern-europe',
+     35: 'ukraine',
+     36: 'ural',
+     37: 'venezuela',
+     38: 'western-australia',
+     39: 'western-europe',
+     40: 'western-united-states',
+     41: 'yakutsk' }
 
-locations = { 
-    'kamchatka': [1510, 150],
-    'yakutsk': [1390, 135],
-    'siberia': [1260, 200],
-    'ural': [1170, 270],
-    'irkutsk': [1360, 280],
-    'mongolia': [1360, 395],
-    'japan': [1530, 395],
-    'china': [1330, 510],
-    'afghanistan': [1140, 430],
-    'india': [1225, 580],
-    'siam': [1355, 630],
-    'middle-east': [1050, 560],
-    
-    'eastern-australia': [1540, 920],
-    'indonesia': [1355, 800],
-    'new-guinea':  [1500, 760],
-    'western-australia': [1445, 970],
-    
-    'madagascar': [1060, 970],
-    'south-africa': [930, 960],
-    'congo': [910, 830],
-    'east-africa': [985, 750],
-    'north-africa': [805, 683],
-    'egypt': [910, 640],
-    
-    'ukraine': [1010, 290],
-    'scandinavia': [880, 200],
-    'southern-europe': [860, 470],
-    'northern-europe': [855, 385],
-    
-    'venezuela': [430, 630],
-    'alaska': [135, 180],
-    'northwest-territory': [300, 180],
-    'alberta': [270, 260],
-    'central-america' : [270, 500],
-    'ontario' : [390, 280],
-    'greenland' : [600, 120],
-    'quebec' : [500, 290],
-    'eastern-united-states' : [400, 420],
-    'western-united-states' : [280, 400],
-    'brazil' : [550, 730],
-    'peru' : [450, 770],
-    'argentina' : [470, 890],
-    'iceland' : [740, 230],
-    'great-britain' : [700, 350],
-    'western-europe' : [720, 540]
-}
+region_neighbors = {
+     0: [35, 36, 6, 15, 21],
+     1: [26, 2, 19],
+     2: [1, 26, 27, 40],
+     3: [4, 28],
+     4: [24, 3, 28, 37],
+     5: [40, 10, 37],
+     6: [31, 15, 0, 32, 36, 22],
+     7: [8, 33, 24],
+     8: [11, 21, 20, 33, 7, 24],
+     9: [23, 38],
+     10: [5, 40, 27, 29],
+     11: [34, 21, 8, 24],
+     12: [14, 25, 30, 39],
+     13: [26, 27, 29, 14],
+     14: [13, 12, 30],
+     15: [6, 31, 21, 0],
+     16: [31, 23, 38],
+     17: [41, 22, 32, 19],
+     18: [19, 22],
+     19: [18, 1, 17, 22, 41],
+     20: [8, 33],
+     21: [8, 11, 35, 0, 15, 34],
+     22: [18, 6, 32, 17, 19],
+     23: [9, 16, 38],
+     24: [39, 11, 8, 7, 4],
+     25: [30, 35, 34, 39, 12],
+     26: [1, 2, 13, 27],
+     27: [2, 10, 13, 26, 29, 40],
+     28: [4, 37, 3],
+     29: [10, 13, 27],
+     30: [35, 25, 12, 14],
+     31: [16, 15, 6],
+     32: [17, 41, 22, 6, 36],
+     33: [7, 8, 20],
+     34: [11, 39, 25, 35, 21],
+     35: [36, 0, 21, 34, 25, 30],
+     36: [32, 6, 0, 35],
+     37: [28, 4, 5],
+     38: [9, 16, 23],
+     39: [12, 24, 34, 25],
+     40: [2, 5, 10, 27],
+     41: [19, 17, 32]}
 
-class Region (object):
-    def __init__(self, name):
-        self.name = name
+region_locations = {
+     0: [1140, 430],
+     1: [135, 180],
+     2: [270, 260],
+     3: [470, 890],
+     4: [550, 730],
+     5: [270, 500],
+     6: [1330, 510],
+     7: [910, 830],
+     8: [985, 750],
+     9: [1540, 920],
+     10: [400, 420],
+     11: [910, 640],
+     12: [700, 350],
+     13: [600, 120],
+     14: [740, 230],
+     15: [1225, 580],
+     16: [1355, 800],
+     17: [1360, 280],
+     18: [1530, 395],
+     19: [1510, 150],
+     20: [1060, 970],
+     21: [1050, 560],
+     22: [1360, 395],
+     23: [1500, 760],
+     24: [805, 683],
+     25: [855, 385],
+     26: [300, 180],
+     27: [390, 280],
+     28: [450, 770],
+     29: [500, 290],
+     30: [880, 200],
+     31: [1355, 630],
+     32: [1260, 200],
+     33: [930, 960],
+     34: [860, 470],
+     35: [1010, 290],
+     36: [1170, 270],
+     37: [430, 630],
+     38: [1445, 970],
+     39: [720, 540],
+     40: [280, 400],
+     41: [1390, 135]}
+
+continent_names = {
+     0: 'africa',
+     1: 'asia',
+     2: 'europe',
+     3: 'north-america',
+     4: 'oceania',
+     5: 'south-america'}
+
+continent_bonuses = {0: 3, 1: 7, 2: 5, 3: 5, 4: 2, 5: 2}
+
+continent_regions = {
+     0: [7, 8, 11, 20, 24, 33],
+     1: [0, 6, 15, 17, 18, 19, 21, 22, 31, 32, 36, 41],
+     2: [12, 14, 25, 30, 34, 35, 39],
+     3: [1, 2, 5, 10, 13, 26, 27, 29, 40],
+     4: [9, 16, 23, 38],
+     5: [3, 4, 28, 37]}
+
+class RiskObject (object):
+    """ Abstract base class for the Continent and Region classes,
+        which provides basic functionality for comparisons and usage in sets. """
+    def __init__(self, init):
+        if isinstance(init, int):
+            self.value = init
+        elif isinstance(init, str):
+            self._from_name(init)
+        assert self.value >= 0 and self.value < len(self.d_names), '{cls}: invalid id ({i})'.format(
+            cls=self.__class__.__name__, i=self.value)
+    def _from_name(self, name):
+        reverse_names = {n: i for i, n in self.d_names.items()}
+        assert name in reverse_names, '{cls}: no such name ({n})'.format(cls=self.__class__.__name__, n=name)
+        self.value = reverse_names[name]
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.value == other.value
+        return False
+    def __ne__(self, other):
+        return not self.__eq__(other)
+    def __hash__(self):
+        return self.value    
     def __repr__(self):
-        return 'Region({r})'.format(r=self.name)
+        return '{cls}("{name}")'.format(cls=self.__class__.__name__, name=self.name())
+
+    def name(self):
+        return self.d_names[self.value]
+
+class Continent (RiskObject):
+    """ The Continent class represents a continent. Continents have names, contain regions,
+        and provide bonuses when a player controls the full continent. """
+    d_names   = continent_names
+    d_bonuses = continent_bonuses
+    d_regions = continent_regions
+    
+    def bonus(self):
+        """ Return the continent bonus for this continent. """
+        return self.d_bonuses[self.value]   
+    def regions(self):
+        """ Return an Allocation of all Regions in this Continent. """
+        return Allocation([Region(r) for r in self.d_regions[self.value]])
+
+class Region (RiskObject):
+    """ The Region is the smallest element on the Risk game board. Regions have names,
+        have neighboring regions, and belong to a continent. """
+    d_names      = region_names
+    d_continents = continent_regions
+    d_neighbors  = region_neighbors
+    d_locations  = region_locations
+    
     def continent(self):
-        global continents
-        for c, regions in continents.items():
-            if self.name in regions: return c
-        raise Exception('no continent defined for region {r}'.format(r=self.name))
+        """ Return the continent to which the regions belongs. This is unique. """
+        for continent, regions in self.d_continents.items():
+            if self.value in regions:
+                return Continent(continent)
     def location(self):
-        global locations
-        return locations[self.name]
+        """ Return the location (in pixels) of the region on the game board. Used for plotting. """
+        return self.d_locations[self.value]
     def neighbors(self):
-        global graph
-        return graph[self.name]
+        """ Return an Allocation containing all neighboring regions. """
+        return Allocation([Region(r) for r in self.d_neighbors[self.value]])
+
+class Allocation (set):
+    """ An Allocation is a collection of regions. It inherits from a set, and has
+        several methods which are specifically useful for risk. """
+    all_continents = set([Continent(x) for x in range(6)])
+    all_regions    = set([Region(x) for x in range(42)])
     
-class Allocation (object):
-    all_regions = set([Region(r) for r in graph.keys()])
-    def __init__(self, regions=[]):
-        if isinstance(regions, int):
-            self.sample(n=regions)
-        elif isinstance(regions, Allocation):
-            self.regions = copy.copy(regions.regions)
+    def __init__(self, a=(), sample=None):
+        """ Initialize, either with a random sample, or from an iterable. """
+        set.__init__(set())
+        if sample is not None:     
+            self.sample(sample)
         else:
-            self.regions = set(regions)
+            self.update(a)
     
     def __repr__(self):
-        return 'Allocation({s})'.format(s=self.regions)
-    
-    def __size__(self):
-        return len(self.regions)
-    
-    def contains(self, region):
-        return region in self.regions
-    
-    def opposite(self):
-        return Allocation(self.all_regions.difference(self.regions))
-    
-    def sample(self, n=21):
-        self.regions = set(random.sample(self.all_regions, n))
-        
-    def choose(self, opposite=False):
-        sample = self.opposite() if opposite else self
-        return random.choice(list(sample.regions))
-    
-    def swap(self):
-        added = self.choose(opposite=True)
-        removed = self.choose()
-        self.regions.remove(removed)
-        self.regions.add(added)
-    
-    def new_armies(self):
-        global continent_bonus
-        retval = max(3, int(math.floor(len(self.regions)/3)))
-        for continent in self.full_continents():
-            retval += continent_bonus[continent]
-        return retval
-    
+        return 'Allocation({l})'.format(l=list(self).__repr__())
+    def __hash__(self):
+        return self.value   
+    def border(self):
+        """ Return an Allocation of the neigboring Regions which are not in this Allocation. """
+        return self.complement().external()
+    def complement(self):
+        """ Return an Allocation containing all Regions which are not in this Allocation. """
+        return Allocation(self.all_regions.difference(self))
+    def continents(self):
+        """ Return a set of all continents on which this Allocation has at least one Region. """
+        return set([region.continent() for region in self])
+    def distant(self):
+        """ Return an Allocation of all regions which are not in and do not border this Allocation. """
+        return self.complement().internal()
+    def external(self):
+        """ Return all Regions of the Allocation which do border with Regions outside the Allocation. """
+        return self.difference(self.internal())
     def full_continents(self):
-        global continents
-        retval = [ ]
-        for continent, regions in continents.items():
-            n_regions = len(regions)
-            n_my_regions = len([r for r in self.regions if r.continent() == continent])
-            if n_regions == (n_my_regions):
-                retval.append(continent)
-        return retval
-    
-    def safe_nodes(self):
-        retval = 0
-        for r in self.regions:
-            safe = True
-            for n in r.neighbors():
-                if not n in [x.name for x in self.regions]:
-                    safe = False
-                    break
-            retval += safe
-        return retval
-    
-    def safe_ratio(self):
-        return float(self.safe_nodes()) / (self.opposite().safe_nodes() + 1)
-    def safe_difference(self):
-        return self.safe_nodes() - self.opposite().safe_nodes()
-    def new_difference(self):
-        return self.new_armies() - self.opposite().new_armies()
-
-
-    
+        """ Return a set of all continents which are fully contained in this Allocation. """
+        return self.all_continents.difference(self.complement().continents())
+    def internal(self):
+        """ Return all Regions of the Allocations which do not border with Regions outside the Allocation. """
+        return Allocation([r for r in self if (len(r.neighbors().difference(self)) == 0)])
+    def pick(self):
+        """ Randomly pick a Region from the Allocation. """
+        return random.choice(list(self)) 
+    def reinforcements(self):
+        """ Calculate the number of reinforcements earned. """
+        return max(3, int(math.floor(len(self)/3))) + sum([continent.bonus() for continent in self.full_continents()])
+    def sample(self, n):
+        """ Randomly construct an allocation of size n. """
+        self.clear()
+        self.update(random.sample(self.all_regions, n))
     def plot(self, color='black'):
         im = plt.imread(os.getcwd() + '/risk.png')
         plt.figure(figsize = (10,15))
         implot = plt.imshow(im)
-        for r in self.regions:
+        for r in self:
             coor = r.location()
             plt.scatter([coor[0]], [coor[1]], s = 150, c=color)
-        plt.show()  
+        plt.show()          
         
